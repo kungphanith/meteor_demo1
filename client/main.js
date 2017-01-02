385f3a9c844703483e25ccc658766ab8e83b63a5
kungphanith <kungphanith79@gmail.com>
2017-01-02 18:13:17 +0700

    initialize project with meteor js

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+Resolutions = new Mongo.Collection('resolutions');
+
+Template.body.helpers({
+  resolutions: function() {
+    return Resolutions.find();
+  }
+});
